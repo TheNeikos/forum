69a75a79cd0946b1b750f74d596e01fab7d69b6f
Marcel Mueller <com2040@gmail.com>
2014-08-05 15:35:04 +0200

    Added validations to user

--- app/user/models.rb
+++ app/user/models.rb
@@ -1,5 +1,13 @@
 require 'sequel'
 
 class User < Sequel::Model
+  plugin :validate_helpers
   one_to_many :user_roles
+
+  def validate
+    super
+    validates_presence [:displayname, :email]
+    validates_min_length 2, :displayname
+    validates_format %r{^.+?@.+?\..+?$}, :email
+  end
 end
