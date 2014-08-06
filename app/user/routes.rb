e7572426ff0fe4e7628b0219dfee7472c5d20e48
Marcel Mueller <com2040@gmail.com>
2014-08-06 21:03:59 +0200

    Removed e-mail login and added password login

--- app/user/routes.rb
+++ app/user/routes.rb
@@ -13,32 +13,12 @@ class Forum < Sinatra::Application
     end
   end
 
-  post "/api/user/request_login" do
-    new_user_data = json_data(:user)
-    verify_user_data(new_user_data)
-    if new_user_data["email"].nil?
-      json_error({email: "is not present"})
-    end
-    user = User[email: new_user_data["email"]]
-    login = user.add_user_login(UserLogin.new( :user => user ))
-    puts login.login_key
-    halt
-    Pony.mail(:to => user.email,
-              :subject => "Your Login Key for Forums",
-              :html_body => haml(:'user/login_email', locals: {
-                user: user,
-                login_key: login.login_key}))
-    {success: true}.to_json
-  end
-
   post "/api/user/login" do
-    json_error({login_key: "is not present"}) if params[:login_key].nil?
-    return if logged_in? # TODO: What should we do if already logged in?
+    return if logged_in? # No need to be logged in twice
 
-    login = UserLogin[:login_key => params[:login_key], :valid => true]
-    json_error({login_key: "is invalid"}) unless login
-    user = login.user
-    login.invalidate
+    user_data = json_data(:user)
+    verify_user_data(user_data)
+    user = User.login_with_password user_data
     UserSession.create(user: user).to_json
   end
 
@@ -67,7 +47,7 @@ class Forum < Sinatra::Application
 
   def verify_user_data data
     data.delete_if do |k,d|
-      !["displayname", "email"].include?(k)
+      !["displayname", "email", "password"].include?(k)
     end
   end
 
