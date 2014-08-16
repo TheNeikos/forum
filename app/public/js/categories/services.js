b8e8d99750c893429a6cf80ffc189b5d99cac5c8
Marcel Mueller <com2040@gmail.com>
2014-08-16 16:42:40 +0200

    Added basic services

--- app/public/js/categories/services.js
+++ app/public/js/categories/services.js
@@ -2,7 +2,7 @@
 angular.module("community.categories", [])
 .service("categories", ["$http", function($http){
     function getAll() {
-        return $http.get("/api/nodes/type/categories")
+        return $http.get("/api/nodes/type/CategoryNode")
     }
 
     return {
--- app/public/js/users/services.js
+++ app/public/js/users/services.js
@@ -1,11 +1,16 @@
 
 angular.module("community.users", [])
+.run(["$rootScope", "users", function($rootScope, users){
+    $rootScope.current_user = users.current_user
+}])
 .service("users", ["$http", function($http){
+    var cur_user = {}
     function getAll() {
         return $http.get("/api/users")
     }
 
     return {
+        current_user: cur_user,
         all: getAll,
     }
 }])
